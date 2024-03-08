b2883b369c1b19257c89202503ecb5931493bc27
felpsalvs <profelipealves@outlook.com>
2024-03-08 14:14:02 -0300

    chore: added sellWeapon function

--- javascript-algorithms-and-data-structures/role-playing-game/script.js
+++ javascript-algorithms-and-data-structures/role-playing-game/script.js
@@ -90,7 +90,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (currentWeapon < weapons.length) {
+  if (currentWeapon < weapons.length - 1) {
     if (gold >= 30) {
       gold -= 30;
       currentWeapon++;
@@ -102,6 +102,15 @@ function buyWeapon() {
     } else {
       text.innerText = "You do not have enough gold to buy a weapon.";
     }
+  } else {
+    text.innerText = "You already have the most powerful weapon!";
+    button2.innerText = "Sell weapon for 15 gold";
+    button2.onclick = sellWeapon;
+  }
+}
+
+function sellWeapon() {
+  if (inventory.length > 1) {
   }
 }
 
